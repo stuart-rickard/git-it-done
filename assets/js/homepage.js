192f4f4d4ef0158727c4bd10cd724cd683abf47b
stuart-rickard <stuart@bau-dev.com>
2021-12-02 08:42:56 -0800

    final code

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,27 +1,8 @@
-var languageButtonsEl = document.getElementById("language-buttons");
 var userFormEl = document.querySelector("#user-form");
+var languageButtonsEl = document.querySelector("#language-buttons");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
-
-var buttonClickHandler = function(e){
-    var language = e.target.getAttribute("data-language");
-    console.log(language);
-}
-
-var getFeaturedRepos = function(language) {
-    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
-  
-    fetch(apiUrl).then(function(response) {
-      if (response.ok) {
-        response.json().then(function(data) {
-          displayRepos(data.items, language);
-        });
-      } else {
-        alert('Error: GitHub User Not Found');
-      }
-    });
-  };
 
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
@@ -38,6 +19,18 @@ var formSubmitHandler = function(event) {
     nameInputEl.value = "";
   } else {
     alert("Please enter a GitHub username");
+  }
+};
+
+var buttonClickHandler = function(event) {
+  // get the language attribute from the clicked element
+  var language = event.target.getAttribute("data-language");
+
+  if (language) {
+    getFeaturedRepos(language);
+
+    // clear old content
+    repoContainerEl.textContent = "";
   }
 };
 
@@ -62,6 +55,23 @@ var getUserRepos = function(user) {
     .catch(function(error) {
       alert("Unable to connect to GitHub");
     });
+};
+
+var getFeaturedRepos = function(language) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  // make a get request to url
+  fetch(apiUrl).then(function(response) {
+    // request was successful
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
 };
 
 var displayRepos = function(repos, searchTerm) {
@@ -110,6 +120,6 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
-// add event listeners to forms
+// add event listeners to form and button container
 userFormEl.addEventListener("submit", formSubmitHandler);
 languageButtonsEl.addEventListener("click", buttonClickHandler);
